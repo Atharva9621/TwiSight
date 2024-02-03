986fced59921328b998219351dc400be456c0baf
Maheshwar098 <107045447+Maheshwar098@users.noreply.github.com>
2024-02-03 10:15:25 +0530

    sentiment analysis module completed

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.homepage,name="homepage"),
     path("explore-tweets", views.explore_tweets,name="explore_tweets"),
-    path("get-tweets",views.get_tweets,name="get-tweets")
+    path("get-tweets-sentiments",views.get_tweet_sentiments,name="get-tweet-sentiments")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .utils import * 
 
 def homepage(req):
     response = JsonResponse({"message":"Homepage"})
@@ -9,6 +10,8 @@ def explore_tweets(req):
     response = JsonResponse({"message":"Tweets explorer page"})
     return response
 
-def get_tweets(req):
-    response = JsonResponse({"message":"Get Tweets endpoint"})
+def get_tweet_sentiments(req):
+    query_params = dict(req.GET)
+    tweet_sentiments = fetch_tweets_and_check_sentiments(query_params)
+    response = JsonResponse({"response":tweet_sentiments})
     return response
